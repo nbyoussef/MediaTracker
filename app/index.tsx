1ce99db6f5ada286077cc668b9903d079ce78c0b
Nizar Youssef <nbyoussef@csus.edu>
2025-05-23 03:31:07 +0300

    Create Hello World

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{
+        fontSize: 50,
+        fontWeight: "bold"
+      }}>Hello World</Text>
     </View>
   );
 }
