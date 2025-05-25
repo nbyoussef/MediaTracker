34b0d820a268922d48112cbba311321ccd81ca47
Nizar Youssef <nbyoussef@csus.edu>
2025-05-25 14:56:49 +0300

    Add search function and show 5 results with titles

--- app/index.tsx
+++ app/index.tsx
@@ -9,9 +9,11 @@ import { StatusBar, View } from "react-native";
 
 export default function Index() {
   const [item, setItem] = useState("");
+  const [movieTitles, setMovieTitles] = useState([]);
   function searchFor(query: string) {
-    console.log(`Searching for ${query}`);
     const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+    const encodedURL = encodeURI(url);
+    console.log(encodedURL);
     const options = {
       method: "GET",
       headers: {
@@ -20,11 +22,27 @@ export default function Index() {
           "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YWFkNjNiMDE3YmFmNzM4YjkwZWMzMjA1MDFjMTg5YSIsIm5iZiI6MTc0ODE1MjYzNi43MTYsInN1YiI6IjY4MzJiMTNjMDhiNTkwN2NkODcyZjhhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MmbAuIZ--mbh0ySRXwL5zsSS4mtjcal9boGm6oinbJk",
       },
     };
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((json) => console.log(json))
-      .catch((err) => console.error(err));
+    async function getData() {
+      try {
+        const response = await fetch(encodedURL, options);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const results = await response.json();
+        console.log(results);
+        return results;
+      } catch (error) {
+        let message = "Unknown Error";
+        if (error instanceof Error) message = error.message;
+        console.log(message);
+      }
+    }
+    getData().then((json) => {
+      const results = json.results.slice(0, 5).map((e: any) => e.title);
+      setMovieTitles(results);
+    });
   }
+
   return (
     <View>
       <StatusBar barStyle="dark-content" />
@@ -43,7 +61,7 @@ export default function Index() {
       </FormControl>
       <Box className="lex justify-center items-center mt-5 px-5">
         <VStack className="w-full" space="md" reversed={false}>
-          <Box className="rounded-xl bg-stone-500 p-5">
+          {/* <Box className="rounded-xl bg-stone-500 p-5">
             <Text className="text-typography-0">Movie Title</Text>
           </Box>
           <Box className="rounded-xl bg-stone-500 p-5">
@@ -54,7 +72,12 @@ export default function Index() {
           </Box>
           <Box className="rounded-xl bg-stone-500 p-5">
             <Text className="text-typography-0">Movie Title</Text>
-          </Box>
+          </Box> */}
+          {movieTitles.map((e, index) => (
+            <Box key={index} className="rounded-xl bg-stone-500 p-5">
+              <Text className="text-typography-0">{e}</Text>
+            </Box>
+          ))}
         </VStack>
       </Box>
     </View>
