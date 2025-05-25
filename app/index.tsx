5d2f0779767201651622a6ddbdad1681e29fc951
Nizar Youssef <nbyoussef@csus.edu>
2025-05-25 08:20:45 +0300

    Add basic list of movie titles

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,39 @@
+import { Box } from "@/components/ui/box";
 import { FormControl } from "@/components/ui/form-control";
 import { SearchIcon } from "@/components/ui/icon";
 import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
 import { StatusBar, View } from "react-native";
 
 export default function Index() {
-	return (
-		<View
-			style={
-				{
-					// flex: 1,
-					// justifyContent: "center",
-					// alignItems: "center",
-				}
-			}
-		>
-			<StatusBar barStyle="dark-content" />
-			<FormControl>
-				<Input variant="rounded" size="lg" className="mt-3 mx-3">
-					<InputSlot className="pl-3">
-						<InputIcon as={SearchIcon} />
-					</InputSlot>
-					<InputField placeholder="Search" />
-				</Input>
-			</FormControl>
-		</View>
-	);
+  return (
+    <View>
+      <StatusBar barStyle="dark-content" />
+      <FormControl>
+        <Input variant="rounded" size="lg" className="mt-3 mx-3">
+          <InputSlot className="pl-3">
+            <InputIcon as={SearchIcon} />
+          </InputSlot>
+          <InputField placeholder="Search" />
+        </Input>
+      </FormControl>
+      <Box className="lex justify-center items-center mt-5 px-5">
+        <VStack className="w-full" space="md" reversed={false}>
+          <Box className="rounded-xl bg-stone-500 p-5">
+            <Text className="text-typography-0">Movie Title</Text>
+          </Box>
+          <Box className="rounded-xl bg-stone-500 p-5">
+            <Text className="text-typography-0">Movie Title</Text>
+          </Box>
+          <Box className="rounded-xl bg-stone-500 p-5">
+            <Text className="text-typography-0">Movie Title</Text>
+          </Box>
+          <Box className="rounded-xl bg-stone-500 p-5">
+            <Text className="text-typography-0">Movie Title</Text>
+          </Box>
+        </VStack>
+      </Box>
+    </View>
+  );
 }
