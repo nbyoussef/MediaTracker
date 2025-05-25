8167376235ac4dd9748ff4aa7b91fad39ee7574f
Nizar Youssef <nbyoussef@csus.edu>
2025-05-25 09:20:41 +0300

    Add search functionality

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,27 @@ import { SearchIcon } from "@/components/ui/icon";
 import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
+import React, { useState } from "react";
 import { StatusBar, View } from "react-native";
 
 export default function Index() {
+  const [item, setItem] = useState("");
+  function searchFor(query: string) {
+    console.log(`Searching for ${query}`);
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YWFkNjNiMDE3YmFmNzM4YjkwZWMzMjA1MDFjMTg5YSIsIm5iZiI6MTc0ODE1MjYzNi43MTYsInN1YiI6IjY4MzJiMTNjMDhiNTkwN2NkODcyZjhhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MmbAuIZ--mbh0ySRXwL5zsSS4mtjcal9boGm6oinbJk",
+      },
+    };
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+      .catch((err) => console.error(err));
+  }
   return (
     <View>
       <StatusBar barStyle="dark-content" />
@@ -15,7 +33,12 @@ export default function Index() {
           <InputSlot className="pl-3">
             <InputIcon as={SearchIcon} />
           </InputSlot>
-          <InputField placeholder="Search" />
+          <InputField
+            placeholder="Search"
+            value={item}
+            onChangeText={(value) => setItem(value)}
+            onSubmitEditing={() => searchFor(item)}
+          />
         </Input>
       </FormControl>
       <Box className="lex justify-center items-center mt-5 px-5">
