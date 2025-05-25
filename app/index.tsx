ac1358a0979028189bf4cea48438192b6e6a037e
Nizar Youssef <nbyoussef@csus.edu>
2025-05-25 07:30:16 +0300

    Add search bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
+import { FormControl } from "@/components/ui/form-control";
+import { SearchIcon } from "@/components/ui/icon";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import { StatusBar, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <StatusBar barStyle="dark-content" />
-    </View>
-  );
+	return (
+		<View
+			style={
+				{
+					// flex: 1,
+					// justifyContent: "center",
+					// alignItems: "center",
+				}
+			}
+		>
+			<StatusBar barStyle="dark-content" />
+			<FormControl>
+				<Input variant="rounded" size="lg" className="mt-3 mx-3">
+					<InputSlot className="pl-3">
+						<InputIcon as={SearchIcon} />
+					</InputSlot>
+					<InputField placeholder="Search" />
+				</Input>
+			</FormControl>
+		</View>
+	);
 }
